b65f40110f999cc1e98da9b1e82f8912143b18aa
ClementCode <mrdominiqueclement@gmail.com>
2024-02-23 20:55:53 +0100

    modification pour tets du cote front sur axios et httClieny

--- src/main/java/com/example/demo/AuthController.java
+++ src/main/java/com/example/demo/AuthController.java
@@ -2,19 +2,20 @@ package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.demo.dto.UserDto;
 
 public class AuthController {
 
-	@PostMapping("/login")
-	public ResponseEntity<UserDto> login(@RequestBody CredentialsDto credentialsDto){
-		
-	UserDto user  = 	userService.login();
-	
-	
-	return ResponseEntity.ok(user);
-	}
+//	@PostMapping("/login")
+//	public ResponseEntity<UserDto> login(@RequestBody CredentialsDto credentialsDto){
+//		
+////	UserDto user  = 	userService.login();
+//	
+//	
+//	return ResponseEntity.ok(user);
+//	}
 	
 	
 	

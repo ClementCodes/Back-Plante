256c71b8bd02aeb5a401fbb81528c9d7fe5b4d26
ClementCode <mrdominiqueclement@gmail.com>
2024-03-11 23:06:44 +0100

    commit test recuperation sur mac

--- src/main/java/main/app/JardinApplication.java
+++ src/main/java/main/app/JardinApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JardinApplication {
-
+// commit test recuperation sur mac
 	public static void main(String[] args) {
 		SpringApplication.run(JardinApplication.class, args);
 
